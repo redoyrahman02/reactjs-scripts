b894a44af94fa6372c1d358bf34f7201f653666e
Md atiqur Rahman <redoyrahman02@gmail.com>
2023-04-10 13:26:56 +0600

    Update timezone.js

--- timezone.js
+++ timezone.js
@@ -2,18 +2,21 @@ import React from 'react';
 import moment from 'moment-timezone';
 
 function TimeZoneList() {
-  const usTimeZones = moment.tz.names().filter(name => name.startsWith('America/'));
+    const ignoreList = ['America/Puerto_Rico', 'America/Virgin', "America/Adak"];
+    const usTimeZones = moment.tz.names().filter(name => {
+        return name.startsWith('America/') && !ignoreList.includes(name);
+        });
 
-  return (
-    <div>
-      <h2>List of US Timezones:</h2>
-      <ul>
-        {usTimeZones.map(timezone => (
-          <li key={timezone}>{timezone}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+        <h2>List of US Timezones:</h2>
+        <ul>
+            {usTimeZones.map(timezone => (
+            <li key={timezone}>{timezone}</li>
+            ))}
+        </ul>
+        </div>
+    );
 }
 
 export default TimeZoneList;
